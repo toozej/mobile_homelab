f05e5534b3b346d8e20a5a843665d35ac1e90d5b
James Tooze <james@toozej.com>
2019-01-06 16:48:04 -0800

    hopefully working self-signed certs

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 DOCKER_BIN=`which docker`
 DOCKER_COMPOSE_BIN=`which docker-compose`
 OPENSSL_BIN=`which openssl`
+CERTUTIL_BIN=`which certutil`
 
 DOCKER_DIR=~/docker
 MOBILE_HOMELAB_DIR=~/docker/mobile_homelab
@@ -31,18 +32,52 @@ for PROJECT in `find . -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*'`; do
     # remove prefix of "./"
     ENTRY=${PROJECT:2}
     # if there's not already a hostfile entry for $PROJECT, then add one
-    if ! grep -q "${ENTRY}.test" /etc/hosts; then
-        echo "127.0.0.1 ${ENTRY}.test" | sudo tee -a /etc/hosts
+    if ! grep -q "$ENTRY.test" /etc/hosts; then
+        echo "127.0.0.1 $ENTRY.test" | sudo tee -a /etc/hosts
     fi
 done
 
-if [ ! -f "$MOBILE_HOMELAB_DIR/traefik/traefik.key" ] || [ ! -f "$MOBILE_HOMELAB_DIR/traefik/traefik.crt" ]; then
+if [ ! -f "$MOBILE_HOMELAB_DIR/traefik/lab.test.key" ] || [ ! -f "$MOBILE_HOMELAB_DIR/traefik/lab.test.crt" ]; then
     echo "generating openssl key & crt for Traefik"
     cd $MOBILE_HOMELAB_DIR/traefik
-    $OPENSSL_BIN req -newkey rsa:2048 -nodes -keyout traefik.key -x509 -days 9999 -out traefik.crt -subj "/C=US/ST=Oregon/L=Portland/O=mobile_homelab/CN=mobile_homelab.test"
+
+    # generate lab.test key
+    $OPENSSL_BIN genrsa -out lab.test.key 2048
+    # verify lab.test key
+    $OPENSSL_BIN rsa -in lab.test.key -noout -text
+
+    # generate CSR for lab.test with SANs
+    $OPENSSL_BIN req -new -out lab.test.csr -config lab.test.conf
+    # verify lab.test CSR
+    $OPENSSL_BIN req -in lab.test.csr -noout -text
+    
+    # generate root CA key
+    $OPENSSL_BIN genrsa -out ca.key 2048
+    # generate root CA cert
+    $OPENSSL_BIN req -new -x509 -key ca.key -out ca.crt -subj "/C=US/ST=Oregon/L=Portland/O=mobile_homelab/CN=[]"
+
+    # sign and create lab.test.crt
+	$OPENSSL_BIN ca -config ca.conf -out lab.test.crt -extfile lab.test.extensions.conf -in lab.test.csr
+    #$OPENSSL_BIN x509 -req -in lab.test.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out lab.test.crt
+    # verify lab.test.crt
+    $OPENSSL_BIN x509 -in lab.test.crt -noout -text
+	$OPENSSL_BIN verify -CAfile ca.crt lab.test.crt
+
+    # create bundle for browser
+    cat lab.test.crt ca.crt > lab.test.bundle.crt
+    
 else
-    echo "traefik.key and traefik.crt already exist, skipping."
+    echo "lab.test.key and lab.test.crt already exist, skipping."
 fi
+
+echo "trusting self-signed traefik certificate"
+[ -d ~/.pki/nssdb ] || mkdir -p ~/.pki/nssdb
+$CERTUTIL_BIN -d sql:$HOME/.pki/nssdb -A -n '*.lab.test certificate authority' -i $MOBILE_HOMELAB_DIR/traefik/lab.test.bundle.crt -t "TCP,TCP,TCP"
+#for PROJECT in `find . -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*'`; do
+#    # remove prefix of "./"
+#    ENTRY=${PROJECT:2}
+#    $CERTUTIL_BIN -d sql:$HOME/.pki/nssdb -A -n '$ENTRY.test' -i $MOBILE_HOMELAB_DIR/traefik/lab.test.crt -t "P,P,P"
+#done
 
 TRAEFIK_NETWORK_OUTPUT=`sudo docker network ls | awk '{print $2}' | grep --color=none traefik`
 if [ "$TRAEFIK_NETWORK_OUTPUT" != "traefik" ]; then
@@ -55,6 +90,7 @@ fi
 for PROJECT in `find $MOBILE_HOMELAB_DIR -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*'`; do
     if [ ! -f "$PROJECT/.do_not_autorun" ]; then
         echo "starting docker-compose project in $PROJECT"
+        sudo $DOCKER_COMPOSE_BIN -f $PROJECT/docker-compose.yml pull --ignore-pull-failures
         sudo $DOCKER_COMPOSE_BIN -f $PROJECT/docker-compose.yml up --build -d
     else
         echo "$PROJECT set to not auto-run, remove $PROJECT/.do_not_autorun if you want to change this."
