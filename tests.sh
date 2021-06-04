eb7770b19abe70ccbfe1d3ff1d8feb4928788d86
James Tooze <toozej@gmail.com>
2021-06-04 14:11:13 -0700

    minor simplications to tests

--- tests.sh
+++ tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -xe 
+
 # curl compose projects to ensure they're actually running
-curl -s -k -L --user traefik:traefik https://traefik.lab.test | grep "Traefik UI" || exit 1
-curl -s -k -L https://jenkins.lab.test | grep "Authentication required" || exit 1
-curl -s -k -L https://www.lab.test | grep Welcome || exit 1
-curl -s -k -L https://portainer.lab.test | grep Portainer || exit 1
-curl -s -k -L https://tick.lab.test | grep Chronograf || exit 1
-echo "if we got to this point the test has finished successfully, exiting." && exit 0
+curl -s -k -L --user traefik:traefik https://traefik.lab.test | grep "Traefik UI"
+curl -s -k -L https://jenkins.lab.test | grep "Authentication required"
+curl -s -k -L https://www.lab.test | grep Welcome
+curl -s -k -L https://portainer.lab.test | grep Portainer
+curl -s -k -L https://tick.lab.test | grep Chronograf
+echo "if we got to this point the test has finished successfully, exiting."
